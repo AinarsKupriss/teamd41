2c78c444a539875a1862fca9e60ce1bc0cd9b18f
artuurskk <juan1337@inbox.lv>
2018-12-04 20:18:33 +0200

    login un register lapas

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Pierakstīšanās</div>
 
-                <div class="card-body">
+    <div class="content">
+
+
+        <!-- Services -->
+        <section id="login">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <h2 class="section-heading text-uppercase">Pierakstīšanās</h2>
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-lg-12 text-center">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">E-pasts</label>
+                            <label for="email" class="col-5 col-form-label text-md-right">E-pasts</label>
 
-                            <div class="col-md-6">
+                            <div class="col-3">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
@@ -26,9 +34,9 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">Parole</label>
+                            <label for="password" class="col-5 col-form-label text-md-right">Parole</label>
 
-                            <div class="col-md-6">
+                            <div class="col-3">
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -39,7 +47,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                  <!--      <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
@@ -49,19 +57,20 @@
                                     </label>
                                 </div>
                             </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
+                        </div> -->
+                        <br>
+                        <div class="form-group row">
+                            <div class="col-4 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     Pierakstīties
                                 </button>
                             </div>
                         </div>
                     </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
+        </section>
+
 </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,91 +1,100 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Reģistrācija</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+    <div class="content">
 
-                        <div class="form-group row">
-                            <label for="firstname" class="col-md-4 col-form-label text-md-right">Vārds</label>
 
-                            <div class="col-md-6">
-                                <input id="firstname" type="text" class="form-control{{ $errors->has('firstname') ? ' is-invalid' : '' }}" name="firstname" value="{{ old('firstname') }}" required autofocus>
+        <!-- Services -->
+        <section id="login">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <h2 class="section-heading text-uppercase">Reģistrācija</h2>
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
 
-                                @if ($errors->has('firstname'))
-                                    <span class="invalid-feedback" role="alert">
+                            <div class="form-group row">
+                                <label for="firstname" class="col-md-5 col-form-label text-md-right">Vārds</label>
+
+                                <div class="col-md-3">
+                                    <input id="firstname" type="text" class="form-control{{ $errors->has('firstname') ? ' is-invalid' : '' }}" name="firstname" value="{{ old('firstname') }}" required autofocus>
+
+                                    @if ($errors->has('firstname'))
+                                        <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('firstname') }}</strong>
                                     </span>
-                                @endif
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="lastname" class="col-md-4 col-form-label text-md-right">Uzvārds</label>
+                            <div class="form-group row">
+                                <label for="lastname" class="col-md-5 col-form-label text-md-right">Uzvārds</label>
 
-                            <div class="col-md-6">
-                                <input id="lastname" type="text" class="form-control{{ $errors->has('lastname') ? ' is-invalid' : '' }}" name="lastname" value="{{ old('lastname') }}" required autofocus>
+                                <div class="col-md-3">
+                                    <input id="lastname" type="text" class="form-control{{ $errors->has('lastname') ? ' is-invalid' : '' }}" name="lastname" value="{{ old('lastname') }}" required autofocus>
 
-                                @if ($errors->has('lastname'))
-                                    <span class="invalid-feedback" role="alert">
+                                    @if ($errors->has('lastname'))
+                                        <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('lastname') }}</strong>
                                     </span>
-                                @endif
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">E-pasts</label>
+                            <div class="form-group row">
+                                <label for="email" class="col-md-5 col-form-label text-md-right">E-pasts</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+                                <div class="col-md-3">
+                                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
 
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
+                                    @if ($errors->has('email'))
+                                        <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">Parole</label>
+                            <div class="form-group row">
+                                <label for="password" class="col-md-5 col-form-label text-md-right">Parole</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                <div class="col-md-3">
+                                    <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
+                                    @if ($errors->has('password'))
+                                        <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
-                                @endif
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Atkārtota parole</label>
+                            <div class="form-group row">
+                                <label for="password-confirm" class="col-md-5 col-form-label text-md-right">Atkārtota parole</label>
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                <div class="col-md-3">
+                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Reģistrēties
-                                </button>
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-3">
+                                    <button type="submit" class="btn btn-primary">
+                                        Reģistrēties
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
+
     </div>
-</div>
 @endsection
