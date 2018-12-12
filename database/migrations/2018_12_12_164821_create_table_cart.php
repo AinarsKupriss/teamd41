b05c2d4da022442cc077653af3e5439e4339f45b
Krisjanis <cepurnieks14@gmail.com>
2018-12-12 23:17:51 +0200

    apdeits yo yo

---
+++ database/migrations/2018_12_12_164821_create_table_cart.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableCart extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('userid');
+            $table->string('orderid');
+            $table->integer('ammount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('table_cart');
+    }
+}
